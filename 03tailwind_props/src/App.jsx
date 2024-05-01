d1362487123a77b9b8eac4a38c9655a87a42c804
MayankMx13 <mayankmishra3214@gmail.com>
2024-05-02 02:10:13 +0530

    intro

---
+++ 03tailwind_props/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-green-400'>tailwind css</h1>
+    </>
+  )
+}
+
+export default App
